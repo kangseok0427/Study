c0d11739512fb6dbf754f6eddf5578ac0bb250f2
kangseok0427 <kangseok0427@gmail.com>
2026-03-19 10:46:25 +0900

    j

---
+++ Java/test/src/javastudy/test02.java
@@ -0,0 +1,14 @@
+package javastudy;
+
+public class test02 {
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 300;
+        num2 = 500;
+        result = num1 + num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + result);
+        System.out.printf("%d + %d = %d",num1, num2, result);
+    }
+}
