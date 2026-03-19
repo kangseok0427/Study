8aa1fd21bf62aabb80c7130fc900b6a6defe1775
kangseok0427 <kangseok0427@gmail.com>
2026-03-19 11:16:42 +0900

    java

--- Java/test/src/javastudy/test02.java
+++ Java/test/src/javastudy/test02.java
@@ -2,13 +2,15 @@ package javastudy;
 
 public class test02 {
     public static void main(String[] args) {
-        int num1, num2, result;
+        int num1, num2, result1, result2;
 
         num1 = 300;
         num2 = 500;
-        result = num1 + num2;
+        result1 = num1 + num2;
+        result2 = num1 * num2;
 
-        System.out.println(num1 + " + " + num2 + " = " + result);
-        System.out.printf("%d + %d = %d",num1, num2, result);
+        System.out.println(num1 + " + " + num2 + " = " + result1);
+        System.out.printf("%d + %d = %d\n",num1, num2, result1);
+        System.out.printf("%d * %d = %d",num1, num2, result2);
     }
 }
