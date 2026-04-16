719c1644bc2bb2f9ca39886df1bc5e0a3ac11f92
kangseok0427 <kangseok0427@gmail.com>
2026-04-16 11:45:57 +0900

    asdf

--- Java/test/src/ai0416/t02.java
+++ Java/test/src/ai0416/t02.java
@@ -1,7 +1,18 @@
 package ai0416;
 
+import java.util.Scanner;
+
 public class t02 {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("정수 입력 : ");
+        int i = s.nextInt();
+
+        if(i % 2 == 0) System.out.println("짝수입니다.");
+        else System.out.println("홀수입니다.");
+
+
+        s.close();
     }
 }
