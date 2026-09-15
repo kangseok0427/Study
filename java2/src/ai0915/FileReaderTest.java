4317c46a20c755307db233f6941e9240ef293ff4
kangseok0427 <kangseok0427@gmail.com>
2026-09-15 16:25:26 +0900

    asdf

---
+++ java2/src/ai0915/FileReaderTest.java
@@ -0,0 +1,26 @@
+package ai0915;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class FileReaderTest {
+    public static void main(String[] args){
+        try(BufferedReader br = new BufferedReader(
+                new FileReader("D:/FileTest/myFile.txt", StandardCharsets.UTF_8))){
+
+            String line = "";
+
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("파일을 찾을 수 없습니다. 파일 경로를 확인해주세요.");
+        }catch(IOException e){
+            System.out.println("파일을 읽는 중 문제가 발생했습니다.");
+        }
+    }
+}
